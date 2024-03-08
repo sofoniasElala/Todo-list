7ceb1ae2ca746606da6cf006eda4b5906090603c
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-03-08 16:58:33 -0500

    add project and task classes
    todo is interchangeable with task. in name only.

---
+++ src/project.js
@@ -0,0 +1,27 @@
+export default class Project {
+    #name;
+    #tasks;
+
+    constructor(name, tasks = []){
+        this.#name = name;
+        this.#tasks = tasks;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+    get name(){
+        return this.#name;
+    }
+    get tasks(){
+        return this.#tasks;
+    }
+
+    addTask(task){
+        this.#tasks.push(task);
+    }
+
+    deleteTask(index){
+        this.#tasks.splice(index, 1);
+    }
+}
---
+++ src/todo.js
@@ -0,0 +1,75 @@
+export default class Task {
+    #title;
+    #description;
+    #notes;
+    #dueDate;
+    #priority;
+    #project;
+    #complete;
+    
+    constructor(title, description, notes, dueDate, priority, project, complete = false){
+        this.#title = title;
+        this.#description = description;
+        this.#notes = notes;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#project = project;
+        this.#complete = complete;
+    }
+
+    set title(title){
+        this.#title = title;
+    }
+
+    get title(){
+        return this.#title;
+    }
+
+    set description(description){
+        this.#description = description;
+    }
+
+    get description(){
+        return this.#description;
+    }
+
+    set notes(notes){
+        this.#notes = notes;
+    }
+
+    get notes(){
+        return this.#notes;
+    }
+
+    set dueDate(dueDate){
+        this.#dueDate = dueDate;
+    }
+
+    get dueDate(){
+        return this.#dueDate;
+    }
+
+    set priority(priority){
+        this.#priority = priority;
+    }
+
+    get priority(){
+        return this.#priority;
+    }
+    
+    set project(project){
+        this.#project = project;
+    }
+
+    get project(){
+        return this.#project;
+    }
+
+    set complete(complete){
+        this.#complete = complete;
+    }
+
+    get complete(){
+        return this.#complete;
+    }
+}
